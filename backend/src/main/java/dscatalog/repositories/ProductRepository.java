cff69972f584ebe739eb2ef11bfb36b68c8af87e
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-01-17 18:41:08 -0300

    Product repository, service, service, resource, GET ok

---
+++ backend/src/main/java/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
